ffc21b07f9235e4bdc1e8211ea59967e1b257838
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 14:35:15 -0300

    Strings

---
+++ 02-tipos-e-operadores/04-String.js
@@ -0,0 +1,11 @@
+/*
+Strings
+ - é um texto
+ - pode usar aspa simples
+ - pode usar aspas duplas
+ - pode usar crase (template literals) para adicionar variaveis
+*/
+
+console.log("Oi, meu nome é Irineu")
+console.log('quero pudim')
+console.log(typeof `infinity`)
