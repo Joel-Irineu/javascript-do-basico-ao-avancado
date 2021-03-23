8a2e2cd1990e93997fc21f17c31961bf17d54731
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 14:22:04 -0300

    operações aritimeticas

---
+++ 02-tipos-e-operadores/02-operaçõesAritimeticas.js
@@ -0,0 +1,14 @@
+/*
+Numbers: Operações aritimeticas
+ - Operação mais feita com os numeros em js
+ - o resultado dessa operação gera um novo number
+ - console.log(2+2)
+ - Segue a mesma logica que a matematica
+*/
+
+console.log(2 + 3) // Soma
+console.log(5 - 12) // Subtração
+console.log(4 * 4) // Divisão
+console.log(5 / 3) // Multiplicação
+console.log(20 % 2) // Resto
+console.log(typeof (21 % 2)) // o resultado gera um number
