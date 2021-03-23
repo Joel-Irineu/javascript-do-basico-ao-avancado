9dfc2ffb0353eeef113d77df24d97817b74ce793
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 15:26:16 -0300

    operadores logicos

---
+++ 02-tipos-e-operadores/08-OperadoresLogicos.js
@@ -0,0 +1,15 @@
+/*
+Booleans: operadores logincos
+ - &&(and) retorna true se ambos os dados da comparação forem true
+ - ||(or) retorna true se ao menos um dos dados forem true
+ - !(not) inverte os valores, true vira false e vice versa
+*/
+console.log(true && true)
+console.log(true && false)
+console.log(true || true)
+console.log(false || true)
+console.log(!true)
+
+console.log(2 > 1 && 3 > 4)
+console.log(2 > 1 || 3 > 4)
+console.log(!(2 > 1 || 3 > 4))
