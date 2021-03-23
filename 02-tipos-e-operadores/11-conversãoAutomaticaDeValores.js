ebbbbae4f0ebdf37a6648c8adb4b8b9e79233060
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 15:42:54 -0300

    Conversão de tipo automatica

---
+++ 02-tipos-e-operadores/11-conversãoAutomaticaDeValores.js
@@ -0,0 +1,7 @@
+/*
+Conversão de tipo automatica
+    - js em determinadas operações converte silenciosamente o tipo do resultado final da operação
+*/
+console.log(typeof ('123' + 4))
+console.log(typeof ('50' - 20))
+console.log(typeof ('a' * 20))
