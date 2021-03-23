8189d6bb17baa6116af6708b59999acad33aeda0
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 15:05:32 -0300

    mais sobre string

---
+++ 02-tipos-e-operadores/05-MiasSobreString.js
@@ -0,0 +1,4 @@
+console.log('primeira linha \nsegunda linha')
+console.log('o meu nome é "irineu"') //aspas dentro de aspas
+console.log(`a soma de 2 + 2 é ${2+2}`) // execulta js
+console.log('o ' + 'meu ' + 'nome ' + 'é irineu')
