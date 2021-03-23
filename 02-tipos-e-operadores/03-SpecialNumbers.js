9952f90fe7f81f125d144cfb6ce9f2d754dde084
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 14:30:43 -0300

    special numbers

---
+++ 02-tipos-e-operadores/03-SpecialNumbers.js
@@ -0,0 +1,11 @@
+/*
+Numbers: Special numbers:
+ - considerados numeros, mas não funcioanm como numeros;
+ - Infinity
+ - -Infinity
+ - NaN(Not A Number)
+*/
+
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(typeof NaN)
