1fc679c6018a3a2e818c35f89bda1f45a5a64462
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 14:13:56 -0300

    tipo number

---
+++ 02-tipos-e-operadores/01-number.js
@@ -0,0 +1,11 @@
+/*
+    Number
+    - Obviamente se trata de numeros
+    - console.log(typtof 13)
+    - console.log(typeof 1.8)
+    - console.log(typeof -5)
+*/
+
+console.log(typeof 5)
+console.log(typeof 12.7)
+console.log(typeof -123)
