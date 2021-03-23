509e8ea5e5c30efd95142ce85871c17be72d7fb7
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 15:14:37 -0300

    boolens

---
+++ 02-tipos-e-operadores/06-Booleans.js
@@ -0,0 +1,8 @@
+/*
+Booleans
+ - Server para guardar uma comparação por exemplo;
+ - os unicos valores possiveis são: true ou false
+*/
+console.log(5 > 2)  // true
+console.log(3 > 10) // false
+console.log(typeof true)
