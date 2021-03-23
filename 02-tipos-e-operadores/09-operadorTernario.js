79d06b15f00875e9c56d8b43addd9ea31e027b37
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 15:35:49 -0300

    operador ternario

---
+++ 02-tipos-e-operadores/09-operadorTernario.js
@@ -0,0 +1,10 @@
+/*
+Booleans: operador ternario
+    - faz um comparativo em apenas uma unica linha
+    - não é muito utilizado e pode deixar o codigo complicado de ler
+    - condição ? 'retornar se true' : 'retornar se false'
+*/
+console.log(true ? 'retornar se true' : 'retornar se false')
+console.log(false ? 'retornar se true' : 'retornar se false')
+console.log(5 > 2 ? 'retornar se true' : 'retornar se false')
+console.log(100 < 20 ? 'retornar se true' : 'retornar se false')
