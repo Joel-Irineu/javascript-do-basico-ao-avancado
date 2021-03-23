54cd2c01537f5aa7e09ec25bd7ad620e75e27179
Joel Irineu <irineu.joel01@gmail.com>
2021-03-23 15:19:50 -0300

    operadores de comparação

---
+++ 02-tipos-e-operadores/07-comparaçõesComBooleans.js
@@ -0,0 +1,20 @@
+/*
+Booleans: comparações
+    - maior que >
+    - menor que <
+    - maior ou igual >=
+    - menor ou ihgaul <=
+    - igual ==
+    - diferente !=
+    - identico ===
+    - muito diferente !==
+*/
+console.log(1 > 2)
+console.log(5 < 10)
+console.log(3 >= 3)
+console.log(5 <= 4)
+console.log(5 == 4)
+console.log("mateus" != "mateus")
+console.log('3' === 3)
+console.log(4 == '4')
+console.log(5 !== '5')
